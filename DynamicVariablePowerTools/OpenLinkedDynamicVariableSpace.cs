4ff54ecace7fd9fb3e5235dbacbbaf130c865d78
Arne Kiesewetter <banane9@live.de>
2025-02-01 13:12:16 +0100

    Add base version of OpenLinkedDynamicVariableSpace from Resonite Integration

---
+++ DynamicVariablePowerTools/OpenLinkedDynamicVariableSpace.cs
@@ -0,0 +1,31 @@
+﻿using FrooxEngine;
+using HarmonyLib;
+using MonkeyLoader.Resonite.UI;
+using MonkeyLoader.Resonite.UI.Inspectors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicVariablePowerTools
+{
+    internal sealed class OpenLinkedDynamicVariableSpace
+        : ResoniteInspectorMonkey<OpenLinkedDynamicVariableSpace, BuildInspectorHeaderEvent>
+    {
+        public override bool CanBeDisabled => true;
+        public override int Priority => HarmonyLib.Priority.First;
+
+        // Needs to be able to handle interfaces for IDynamicVariable
+        public OpenLinkedDynamicVariableSpace() : base(typeof(DynamicVariableBase<>))
+        { }
+
+        protected override void Handle(BuildInspectorHeaderEvent eventData)
+        {
+            if (Traverse.Create(eventData.Worker).Field("handler").Field("_currentSpace").GetValue() is not DynamicVariableSpace space)
+                return;
+
+            InspectorUIHelper.BuildHeaderOpenParentButtons(eventData.UI, space);
+        }
+    }
+}
