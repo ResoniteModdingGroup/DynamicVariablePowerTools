64e621a557c65d180eda8c9655d08bab3be99558
Arne Kiesewetter <banane9@live.de>
2026-04-25 00:29:18 +0200

    Finish implementation of dynamic variable inspector member actions

--- DynamicVariablePowerTools/ContextMenu/DVMA.References.cs
+++ DynamicVariablePowerTools/ContextMenu/DVMA.References.cs
@@ -7,55 +7,122 @@ namespace DynamicVariablePowerTools.ContextMenu
 {
     internal sealed partial class DynamicVariableMemberActions
     {
-        private static void CreateSyncRefItems<T>(GenerationEvent eventData)
+        private static ButtonEventHandler GetDriveSyncRefFromVariable<T>(GenerationEvent eventData, SyncRef<T> syncRefTarget, string variable)
             where T : class, IWorldElement
-        {
-            if (eventData.Target is not SyncRef<T> syncRefTarget)
-                return;
-
-            var menuItem = eventData.ContextMenu.AddItem(Mod.GetLocaleString("Source", "type", "DynamicReference"), SourceIcon, SourceColor);
-
-            menuItem.Button.LocalPressed += (sender, args) =>
+            => (button, args) =>
             {
-                var slot = eventData.Target.FindNearestParent<Slot>();
-                var dynamicReference = slot.AttachComponent<DynamicReference<T>>();
-                dynamicReference.TargetReference.Target = syncRefTarget;
-
+                syncRefTarget.DriveFromVariable(variable);
                 eventData.CloseContextMenu();
             };
 
-            if (syncRefTarget.IsLinked)
-                return;
-
-            menuItem = eventData.ContextMenu.AddItem(Mod.GetLocaleString("DriveFrom"), (Uri)null!, RadiantUI_Constants.Sub.PURPLE);
-
-            menuItem.Button.LocalPressed += (button, args) =>
+        private static ButtonEventHandler GetOfferSyncRefDriveActions<T>(GenerationEvent eventData, SyncRef<T> syncRefTarget)
+            where T : class, IWorldElement
+            => (button, args) =>
             {
+                eventData.CloseContextMenu();
+
                 button.Slot.StartTask(async () =>
                 {
                     if (await eventData.OpenContextMenuAsync(args.source.Slot) is null)
                         return;
 
-                    var slot = eventData.Target.FindNearestParent<Slot>();
+                    eventData.ContextMenu.AddItem(Mod.GetLocaleString("Drive.FromBlank"), DriveIcon, DriveColor)
+                        .Button.LocalPressed += GetDriveSyncRefFromVariable(eventData, syncRefTarget, string.Empty);
 
-                    var menuItem2 = eventData.ContextMenu.AddItem(Mod.GetLocaleString("Drive.FromBlank"), (Uri)null!, RadiantUI_Constants.Sub.PURPLE);
-                    menuItem2.Button.LocalPressed += (button2, args2) =>
+                    foreach (var variable in GetAvailableVariableOptions<T>(eventData.Slot!))
                     {
-                        syncRefTarget.DriveFromVariable("");
-                        eventData.CloseContextMenu();
-                    };
-
-                    foreach (var option in slot.GetAvailableVariableIdentities<T>())
-                    {
-                        var menuItem3 = eventData.ContextMenu.AddItem($"{option.Space.SpaceName}/{option.Name}", (Uri)null!, RadiantUI_Constants.Sub.PURPLE);
-                        menuItem3.Button.LocalPressed += (button2, args2) =>
-                        {
-                            syncRefTarget.DriveFromVariable($"{option.Space.SpaceName}/{option.Name}");
-                            eventData.CloseContextMenu();
-                        };
+                        eventData.ContextMenu.AddItem(Mod.GetLocaleString("Drive.FromVariable", "variable", variable), DriveIcon, DriveColor)
+                            .Button.LocalPressed += GetDriveSyncRefFromVariable(eventData, syncRefTarget, variable);
                     }
                 });
             };
+
+        private static ButtonEventHandler GetOfferSyncRefReferenceActions<T>(GenerationEvent eventData, SyncRef<T> syncRefTarget)
+            where T : class, IWorldElement
+            => (button, args) =>
+            {
+                eventData.CloseContextMenu();
+
+                button.Slot.StartTask(async () =>
+                {
+                    if (await eventData.OpenContextMenuAsync(args.source.Slot) is null)
+                        return;
+
+                    eventData.ContextMenu.AddItem(Mod.GetLocaleString("Reference.Blank"), ReferenceIcon, ReferenceColor)
+                        .Button.LocalPressed += GetReferenceSyncRefForVariable(eventData, syncRefTarget, string.Empty);
+
+                    var spaces = eventData.Slot!
+                        .GetAvailableSpaces(SpaceHasName);
+
+                    foreach (var space in spaces)
+                    {
+                        eventData.ContextMenu.AddItem(Mod.GetLocaleString("Reference.InSpace", "space", space.SpaceName), ReferenceIcon, ReferenceColor)
+                            .Button.LocalPressed += GetReferenceSyncRefForVariable(eventData, syncRefTarget, $"{space.SpaceName}/");
+                    }
+                });
+            };
+
+        private static ButtonEventHandler GetOfferSyncRefSourceActions<T>(GenerationEvent eventData, SyncRef<T> syncRefTarget)
+            where T : class, IWorldElement
+            => (button, args) =>
+            {
+                eventData.CloseContextMenu();
+
+                button.Slot.StartTask(async () =>
+                {
+                    if (await eventData.OpenContextMenuAsync(args.source.Slot) is null)
+                        return;
+
+                    eventData.ContextMenu.AddItem(Mod.GetLocaleString("Source.Blank"), SourceIcon, SourceColor)
+                        .Button.LocalPressed += GetSourceSyncRefForVariable(eventData, syncRefTarget, string.Empty);
+
+                    var spaces = eventData.Slot!
+                        .GetAvailableSpaces(SpaceHasName);
+
+                    foreach (var space in spaces)
+                    {
+                        eventData.ContextMenu.AddItem(Mod.GetLocaleString("Source.InSpace", "space", space.SpaceName), SourceIcon, SourceColor)
+                            .Button.LocalPressed += GetSourceSyncRefForVariable(eventData, syncRefTarget, $"{space.SpaceName}/");
+                    }
+                });
+            };
+
+        private static ButtonEventHandler GetReferenceSyncRefForVariable<T>(GenerationEvent eventData, ISyncRef<T> syncRefTarget, string variable)
+            where T : class, IWorldElement
+            => (button, args) =>
+            {
+                var dynamicReference = syncRefTarget.FindNearestParent<Slot>().AttachComponent<DynamicReferenceVariable<ISyncRef<T>>>();
+                dynamicReference.VariableName.Value = variable;
+                dynamicReference.Reference.Target = syncRefTarget;
+
+                eventData.CloseContextMenu();
+            };
+
+        private static ButtonEventHandler GetSourceSyncRefForVariable<T>(GenerationEvent eventData, SyncRef<T> syncRefTarget, string variable)
+            where T : class, IWorldElement
+            => (button, args) =>
+            {
+                syncRefTarget.SyncWithVariable(variable);
+                eventData.CloseContextMenu();
+            };
+
+        private static void OfferSyncRefActions<T>(GenerationEvent eventData)
+            where T : class, IWorldElement
+        {
+            if (eventData.Target is not SyncRef<T> syncRefTarget)
+                return;
+
+            if (!syncRefTarget.IsLinked)
+            {
+                eventData.ContextMenu.AddItem(Mod.GetLocaleString("Drive"), DriveIcon, DriveColor)
+                    .Button.LocalPressed += GetOfferSyncRefDriveActions(eventData, syncRefTarget);
+            }
+
+            eventData.ContextMenu.AddItem(Mod.GetLocaleString("Source", "type", "DynamicReference"), SourceIcon, SourceColor)
+                .Button.LocalPressed += GetOfferSyncRefSourceActions(eventData, syncRefTarget);
+
+            eventData.ContextMenu.AddItem(Mod.GetLocaleString("Reference"), ReferenceIcon, ReferenceColor)
+                .Button.LocalPressed += GetOfferSyncRefReferenceActions(eventData, syncRefTarget);
         }
     }
 }
