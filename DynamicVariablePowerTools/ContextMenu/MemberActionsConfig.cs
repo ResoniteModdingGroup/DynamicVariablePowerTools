6af5834e5b2327fd196f2a58d4de59964a15dc2c
Arne Kiesewetter <banane9@live.de>
2026-08-06 19:19:12 +0200

    Add offset config for open linked space button

--- DynamicVariablePowerTools/ContextMenu/MemberActionsConfig.cs
+++ DynamicVariablePowerTools/ContextMenu/MemberActionsConfig.cs
@@ -4,18 +4,18 @@ namespace DynamicVariablePowerTools.ContextMenu
 {
     internal sealed class MemberActionsConfig : ConfigSection
     {
-        private readonly DefiningConfigKey<bool> _allowDowncastToDriveKey = new("AllowDowncastToDrive", "Allows downcasting the type of the dynamic variable to the type required by the target reference.", () => true);
-        private readonly DefiningConfigKey<bool> _allowUpcastToDriveKey = new("AllowUpcastToDrive", "Allows upcasting the type of the dynamic variable to the type required by the target reference.", () => false);
+        private readonly DefiningConfigKey<bool> _allowDowncastToDrive = new("AllowDowncastToDrive", "Allows downcasting the type of the dynamic variable to the type required by the target reference.", () => true);
+        private readonly DefiningConfigKey<bool> _allowUpcastToDrive = new("AllowUpcastToDrive", "Allows upcasting the type of the dynamic variable to the type required by the target reference.", () => false);
 
         /// <summary>
         /// Gets whether to allow downcasting the type of the dynamic variable's value to the type required by the target field.
         /// </summary>
-        public bool AllowDowncastToDrive => _allowDowncastToDriveKey;
+        public bool AllowDowncastToDrive => _allowDowncastToDrive;
 
         /// <summary>
         /// Gets whether to allow upcasting the type of the dynamic variable's value to the type required by the target field.
         /// </summary>
-        public bool AllowUpcastToDrive => _allowUpcastToDriveKey;
+        public bool AllowUpcastToDrive => _allowUpcastToDrive;
 
         /// <inheritdoc/>
         public override string Id => "MemberActions";
--- DynamicVariablePowerTools/DebugInfoConfig.cs
+++ DynamicVariablePowerTools/DebugInfoConfig.cs
@@ -1,11 +1,19 @@
 ﻿using MonkeyLoader.Configuration;
+using MonkeyLoader.Resonite.Configuration;
+using MonkeyLoader.Resonite.UI.Inspectors;
 
 namespace DynamicVariablePowerTools
 {
-    internal sealed class DebugInfoConfig : ConfigSection
+    internal sealed class DebugInfoConfig : SingletonConfigSection<DebugInfoConfig>
     {
         private readonly DefiningConfigKey<bool> _enableLinkedComponentHierarchy = new("EnableLinkedComponentHierarchy", "Allow generating a hierarchical list of all dynamic variable components linked to a space.", () => true);
         private readonly DefiningConfigKey<bool> _enableLinkedVariablesList = new("EnableLinkedVariablesList", "Allow generating a list of all dynamic variable definitions linked to a space.", () => true);
+
+        private readonly DefiningConfigKey<int> _openLinkedSpaceOffset = new("OpenLinkedSpaceOffset", "The Order Offset of the buttons to open the linked dynamic variable space on Inspector Headers of dynamic variable components. Higher is further right.", () => 0)
+        {
+            DefaultInspectorHeaderConfig.OffsetRange,
+            DefaultInspectorHeaderConfig.MakeOffsetRangeShare(0)
+        };
 
         public override string Description => "Contains the options for the available debug info buttons on DynamicVariableSpace Components.";
 
@@ -14,6 +22,8 @@ namespace DynamicVariablePowerTools
         public bool EnableLinkedVariablesList => _enableLinkedVariablesList;
 
         public override string Id => "DebugInfo";
+
+        public ConfigKeySessionShare<int, long> OpenLinkedSpaceOffset => _openLinkedSpaceOffset.Components.Get<ConfigKeySessionShare<int, long>>();
 
         public override Version Version { get; } = new Version(1, 0, 0);
     }
